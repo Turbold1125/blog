a19ded8e4a66c46697770e3135b84f0c7388dd0c
Turbold1125 <turbold1125@gmail.com>
2024-01-23 18:47:01 +0800

    signin api

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
-import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsGit } from "react-icons/bs";
+import {
+	BsFacebook,
+	BsInstagram,
+	BsTwitter,
+	BsGithub,
+	BsGit,
+} from "react-icons/bs";
 export default function FooterComponent() {
 	return (
 		<Footer container className="border border-t-8 border-teal-500">
@@ -85,11 +91,11 @@ export default function FooterComponent() {
 						year={new Date().getFullYear()}
 					/>
 					<div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-					    <Footer.Icon href="#" icon={BsFacebook}/>
-					    <Footer.Icon href="#" icon={BsInstagram}/>
-					    <Footer.Icon href="#" icon={BsTwitter}/>
-					    <Footer.Icon href="#" icon={BsGithub}/>
-					    <Footer.Icon href="#" icon={BsFacebook}/>
+						<Footer.Icon href="#" icon={BsFacebook} />
+						<Footer.Icon href="#" icon={BsInstagram} />
+						<Footer.Icon href="#" icon={BsTwitter} />
+						<Footer.Icon href="#" icon={BsGithub} />
+						<Footer.Icon href="#" icon={BsFacebook} />
 					</div>
 				</div>
 			</div>
